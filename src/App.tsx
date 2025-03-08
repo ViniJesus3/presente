3798b22b95b5f08a2632e0d234c40da6774efb51
ViniJesus3 <vini.clash1342@gmail.com>
2025-03-08 20:15:42 -0300

    botao

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,32 @@ import "./styles/global.css";
 import { Musica } from "./components/Musica/Musica";
 import { Carrossel } from "./components/Carrosel/Carrosel";
 import { Contador } from "./components/Contador/Contador";
+import React, { useState } from "react";
 
 function App() {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(true);
+  };
+
   return (
     <>
-      <main>
-        <Musica />
-        <Carrossel />
-        <Contador />
-      </main>
+      {/* Botão */}
+      {!isClicked && (
+        <button className="button" onClick={handleClick}>
+          Clique aqui ❤️
+        </button>
+      )}
+
+      {/* Condicional para mostrar a aplicação */}
+      {isClicked && (
+        <main>
+          <Musica />
+          <Carrossel />
+          <Contador />
+        </main>
+      )}
     </>
   );
 }
